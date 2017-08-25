e2107783a3edb3459a89a9b64b7c760098818ee4
Teloi <99101154@qq.com>
2017-08-25 17:46:01 +0800

    fix build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 };
 
 export class Environment {
-  // static readonly serverUrl = 'http://tjserver.esoosoft.com/';
-  // static readonly serverApiUrl = Environment.serverUrl + 'api/';
-  // static readonly clientUrl = 'http://tongji.esoosoft.com/';
-  // static readonly clientSrcUrl = Environment.clientUrl;
+  static readonly serverUrl = 'http://server.teloi.cn/';
+  static readonly serverApiUrl = Environment.serverUrl + 'api/';
+  static readonly clientUrl = 'http://www.teloi.cn/';
+  static readonly clientSrcUrl = Environment.clientUrl;
 }
